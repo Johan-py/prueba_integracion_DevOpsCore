6f55530917cd0dc7abb8cc65de2fd54b845f8cf8
Vlady610 <cvladimir610@gmail.com>
2026-03-27 23:47:22 -0400

    prueba 5

--- frontend/src/app/prueba/page.tsx
+++ frontend/src/app/prueba/page.tsx
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Biembenido</title>
-</head>
-<body>
-    
-</body>
-</html>
+// frontend/src/app/PRUEBA/page.tsx
+export default function PruebaPage() {
+  return (
+    <div>
+      <h1>Bienvenido</h1>
+      <p>Esta es mi página de prueba</p>
+    </div>
+  );
+}
