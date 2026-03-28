6c34a97d14d1a9e1375e6885409136222d901a1c
Jofre Ticona Plata <202202581@est.umss.edu>
2026-03-28 19:06:50 -0400

    refactor(backend): usar puerto desde variables de entorno

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ app.post('/api/users', (req, res) => {
 
 app.use('/api/publicaciones', multimediaRoutes)
 
-const PORT = 5000
+const PORT = Number(process.env.PORT) || 5000
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
