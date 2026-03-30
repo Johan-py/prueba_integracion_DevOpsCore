5bc7894888dd5e790e44a512c78d1795ff681c10
Esther Romero Menacho <romeromenachoesther@gmail.com>
2026-03-30 00:54:14 -0400

    feat(filtrosHomePage): se agrego logica de bd

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,12 +1,16 @@
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-// Forzamos a que Prisma sepa que NO estamos en un entorno Edge/Client
-const prisma = new PrismaClient({
-  __internal: {
-    engine: {
-      endpoint: process.env.DATABASE_URL
-    }
-  }
-} as any); 
+// 1. Configurar el pool de conexión a PostgreSQL (Neon)
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
 
+// 2. Crear el adaptador
+const adapter = new PrismaPg(pool);
+
+// 3. Instanciar el cliente pasando el adaptador
+export const prisma = new PrismaClient({ adapter });
+
+// --- AÑADE ESTO AQUÍ ABAJO ---
 export default prisma;
