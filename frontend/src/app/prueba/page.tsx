9642ea7d2660bbf0e3c2e82649e801645522fbfb
Vlady610 <cvladimir610@gmail.com>
2026-03-30 14:59:22 -0400

    Tarea 1: En la parte superior se añádio los estados (resumen, pagar, confirmacion)

--- frontend/src/app/prueba/page.tsx
+++ frontend/src/app/prueba/page.tsx
@@ -5,11 +5,23 @@ export default function ResumenCompra() {
   return (
     <div>
       {/* ================ PARTE SUPERIOR ================ */}
-      <div>
-        <h1>Resumen de Compra</h1>
+      <div className="flex justify-center gap-4 mb-6">
+        <div className="px-4 py-2 bg-blue-500 text-white rounded-lg">
+          Resumen
+        </div>
+        <div className="px-4 py-2 bg-gray-300 text-gray-700 rounded-lg">
+          Pagar
+        </div>
+        <div className="px-4 py-2 bg-gray-300 text-gray-700 rounded-lg">
+          Confirmación
+        </div>
       </div>
       {/* ================ FIN PARTE SUPERIOR ================ */}
-
+      <div className="bg-white p-6 rounded-xl shadow-md">
+        <h1 className="text-2xl font-bold text-center">
+          Resumen de compra
+        </h1>
+      </div>
       {/* ================ PARTE INTERMEDIA ================ */}
       <div>
       </div>
