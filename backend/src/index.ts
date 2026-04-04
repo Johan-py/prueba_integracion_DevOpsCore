2146f55c43766c8a020734230b89036ff5268efe
Jorge_Paniagua <jorgeluispaniagua2607@gmail.com>
2026-04-03 22:10:12 -0400

    fix: universal cors configuration for vercel compatibility

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -37,7 +37,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: ['*'],
+    origin: '*',
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
